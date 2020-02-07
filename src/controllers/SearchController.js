e524164e3db585e3e4c5e99eef41737fc6926c98
jose pena <josepena@planeta.inf.br>
2020-02-07 11:12:29 -0300

    The API is returning the weather

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,13 +2,19 @@ const axios = require("axios");
 
 module.exports = {
     async searchTemp(req, res) {
-        let { city_name } = req.params;
-        console.log(city_name);
-        city_name[0].toUpperCase();
+        try {
+            const { city_name } = req.body;
 
-        const tempResponse = await axios.get(
-            "api.openweathermap.org/data/2.5/weather?q=${city_name}"
-        );
-        return res.json(tempResponse);
+            city_name[0].toUpperCase();
+
+            let tempResponse = await axios.get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=87a219ebc928e209b94f1e455f4a2917&units=metric`
+            );
+            let data = tempResponse.data;
+
+            return res.json(data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
