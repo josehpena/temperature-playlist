4a804e720fc7aef39d25029fd84f9702d05848c6
jose pena <josepena@planeta.inf.br>
2020-02-07 16:12:17 -0300

    SearchPlaylist (just to save)

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,20 +1,44 @@
 const axios = require("axios");
 
 module.exports = {
-    async searchTemp(req, res) {
+    async searchTemp(req) {
         try {
             const { city_name } = req.body;
 
             city_name[0].toUpperCase();
 
             let tempResponse = await axios.get(
-                `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=87a219ebc928e209b94f1e455f4a2917&units=metric`
+                `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=b77e07f479efe92156376a8b07640ced&units=metric`
             );
-            let data = tempResponse.data;
+            let temperature = tempResponse.data.main.temp;
 
-            return res.json(data);
+            return searchPlaylist(temperature);
+
+            //return res.json(playlist);
         } catch (error) {
             console.log(error);
         }
     }
 };
+
+async function searchPlaylist(temp, res) {
+    if (temp > 30.0) return console.log("Party Tracks");
+    else if (temp >= 15.0) return console.log("Pop");
+    else if (temp >= 10.0) return console.log("Rock");
+    else return console.log("Classical");
+
+    /*
+                    const axiosInstance = axios.create({
+                        baseURL: "https://api.spotify.com/v1/playlists/4oq9wx7oNEasiqNMxTB29G/tracks",
+                        timeout: 5000,
+                        headers: {
+                            Authorization: "Bearer 08c1a6be652e4fdca07f1815bfd167e4",
+                            "Content-Type": "application/json"
+                        }
+                    });
+
+                    let data = await axiosInstance.get().catch(err => {});
+                    console.log(data);
+                    return res.json(data);
+                    https://api.spotify.com/v1/playlists/{playlist_id}*/
+}
